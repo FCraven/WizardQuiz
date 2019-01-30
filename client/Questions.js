5c8c160cbbbe393b2c258d96c9255d3180fb38a3
FCraven <craven.francis@gmail.com>
2019-01-29 20:03:00 -0500

    questions from questions.json put on component state

--- client/Questions.js
+++ client/Questions.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
+import questions from '../data/questions.json'
 
-
-//List of Questions
 
 export default class Questions extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      questions
+    }
+  }
+
   render () {
+    console.log(this.state.questions)
     return (
       <div>
         {/* start here */}
